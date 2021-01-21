ca2abf42d57ac4367dc9dd620c713f4a4cbbf504
Joe Abele <josephabele2@gmail.com>
2021-01-21 16:02:14 -0500

    some commit

--- main.js
+++ main.js
@@ -19,3 +19,39 @@ function simpleSlider() {
 };
 
 simpleSlider();
+
+
+
+let specials = document.getElementById("specials");
+let menu= document.getElementById("menu");
+let menuItem1=document.getElementById("menu-item1");
+let menuItem2=document.getElementById("menu-item2");
+let menuItem3=document.getElementById("menu-item3");
+let foodMenu = document.getElementById("food-menu");
+let contact= document.getElementById("social");
+let navcheck = document.getElementById("navcheck");
+
+
+
+
+
+
+menuItem1.addEventListener("click" ,function(){
+  foodMenu.scrollIntoView({behavior: "smooth"});
+  navcheck.checked = false;
+});
+menuItem2.addEventListener("click" ,function(){
+  specials.scrollIntoView({behavior: "smooth"});
+  navcheck.checked = false;
+});
+menuItem3.addEventListener("click" ,function(){
+  contact.scrollIntoView({behavior: "smooth"});
+  navcheck.checked = false;
+});
+
+
+/*
+element.scrollIntoView({behavior: "smooth"});
+
+*/
+
