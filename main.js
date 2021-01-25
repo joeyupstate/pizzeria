a09c259c17779a571a2201c670ea32b207068826
Joe Abele <josephabele2@gmail.com>
2021-01-25 15:22:05 -0500

    monday 2

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 
+/* slider */
+;function simpleSlider() {
 
+  var options = arguments[0] || {};
+  var sliderSelector = options.selector || '.js-simple-slider';
+  var sliderHiddenClass = options.classHidden || 'is-hidden';
+  var sliderInterval = options.interval || 6000;
+
+  var slider = document.querySelector(sliderSelector);
+  var slidesCount = slider.children.length;
+  var i = 0;
+
+  setInterval(function(){
+    slider.children[i].classList.add(sliderHiddenClass);
+    i++;
+    if(i >= slider.children.length) {i = 0;}
+    slider.children[i].classList.remove(sliderHiddenClass);
+  }, sliderInterval);
+
+};
+
+simpleSlider();
+
+/* menu scroll function */
 
 let specials = document.getElementById("specials");
 let menu= document.getElementById("menu");
