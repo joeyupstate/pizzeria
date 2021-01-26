274ec220392075adf84d4b87319f66aec75bae09
Joe Abele <josephabele2@gmail.com>
2021-01-26 15:01:33 -0500

    idk

--- main.js
+++ main.js
@@ -1,26 +1,5 @@
 
-/* slider */
-;function simpleSlider() {
 
-  var options = arguments[0] || {};
-  var sliderSelector = options.selector || '.js-simple-slider';
-  var sliderHiddenClass = options.classHidden || 'is-hidden';
-  var sliderInterval = options.interval || 6000;
-
-  var slider = document.querySelector(sliderSelector);
-  var slidesCount = slider.children.length;
-  var i = 0;
-
-  setInterval(function(){
-    slider.children[i].classList.add(sliderHiddenClass);
-    i++;
-    if(i >= slider.children.length) {i = 0;}
-    slider.children[i].classList.remove(sliderHiddenClass);
-  }, sliderInterval);
-
-};
-
-simpleSlider();
 
 /* menu scroll function */
 
@@ -40,6 +19,7 @@ let navcheck = document.getElementById("navcheck");
 
 menuItem1.addEventListener("click" ,function(){
   foodMenu.scrollIntoView({behavior: "smooth"});
+
   navcheck.checked = false;
 });
 menuItem2.addEventListener("click" ,function(){
